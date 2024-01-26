0ba2d4eb54113b9ebd907cede238cab538c4e4ec
Love-Fromage <mathecool22@gmail.com>
2024-01-26 17:06:21 -0500

    dernier commit de la journee

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "./constants.h"
+
+SDL_Window* window = NULL;
+
+int initialize_window(void){
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        fprintf(stderr, "Error initializing SDL.\n");
+        return FALSE;
+    }
+
+     window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+     );
+    return TRUE;
+}
 
 int main(){
-    printf("Game is running...\n");
+    initialize_window();
     return 0;
 }
