0d21cbd344e3fde51079347f78b12d685947352d
Love-Fromage <mathecool22@gmail.com>
2024-01-28 13:36:47 -0500

    ajout de la ball

--- src/main.c
+++ src/main.c
@@ -6,6 +6,13 @@ int game_is_running = FALSE;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
+struct ball {
+    float x;
+    float y;
+    float width;
+    float height; 
+} ball;
+
 int initialize_window(){
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
         fprintf(stderr, "Error initializing SDL.\n");
@@ -55,12 +62,30 @@ void process_input(){
 
 }
 
-void update(){
+void setup(){
+    ball.x = 20;
+    ball.y = 20;
+    ball.width = 15;
+    ball.height = 15;
+}
 
+void update(){
+    //TODO:
 }
 
 void render(){
+    SDL_SetRenderDrawColor(renderer, 0,0,0,250);
+    SDL_RenderClear(renderer);
 
+    // Draw a rectangle
+    SDL_Rect ball_rect = {
+        ball.x,
+        ball.y,
+        ball.width,
+        ball.height
+    };
+
+    SDL_RenderPresent(renderer);
 }
 
 void destroy_window(){
@@ -72,7 +97,7 @@ void destroy_window(){
 int main(){
     game_is_running = initialize_window();
 
-    // setup();
+    setup();
 
     while(game_is_running){
         process_input();
